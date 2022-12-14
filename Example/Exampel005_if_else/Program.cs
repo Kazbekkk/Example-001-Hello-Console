17ef2cb8a8a17c41c64e2b8770f0d92fe0c71113
Kazbek <bender.rodrigiz2014@gmail.com>
2022-12-14 18:04:21 +0300

    if_else

---
+++ Example/Exampel005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователяж: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
